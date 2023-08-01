04b21a848cd9c6624c3fead362d8b417410b4595
Ilya <61752920+ilyadudiak@users.noreply.github.com>
2023-08-01 17:47:30 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// let shshhs = $('.header');
+// $(document).on('ready', function () {
+//     $('.header-container').addClass('header-container__absolute');
+//     $(window).on('scroll', () => {
+//         console.log($(window).scrollTop());
+//         if ($(window).scrollTop() > 50) {
+//             $('.header-container').removeClass('header-container__absolute');
+//             $('.header-container').addClass('header-container__sticky');
+
+//         } else {
+//             $('.header-container').addClass('header-container__absolute');
+//             $('.header-container').removeClass('header-container__sticky');
+//         }
+//     })
+
+
+// })

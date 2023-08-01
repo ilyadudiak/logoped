5c715a4f08213ae44029e9ee4e2a286021c3074c
Ilya <61752920+ilyadudiak@users.noreply.github.com>
2023-08-01 22:59:19 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
-// let shshhs = $('.header');
-// $(document).on('ready', function () {
-//     $('.header-container').addClass('header-container__absolute');
-//     $(window).on('scroll', () => {
-//         console.log($(window).scrollTop());
-//         if ($(window).scrollTop() > 50) {
-//             $('.header-container').removeClass('header-container__absolute');
-//             $('.header-container').addClass('header-container__sticky');
 
-//         } else {
-//             $('.header-container').addClass('header-container__absolute');
-//             $('.header-container').removeClass('header-container__sticky');
-//         }
-//     })
+$(document).on('ready', function () {
+    $('.header').addClass('header-unactive');
+    $(window).on('scroll', () => {
+        console.log($(window).scrollTop());
+        if ($(window).scrollTop() > 50) {
+            $('.header').removeClass('header-unactive');
+            $('.header').addClass('header-active');
+
+        } else {
+            $('.header').addClass('header-unactive');
+            $('.header').removeClass('header-active');
+        }
+    })
 
 
-// })
+})
